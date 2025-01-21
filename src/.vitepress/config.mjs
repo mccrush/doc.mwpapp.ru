3393ad447a0d508c2033cb0bdd6c0d5a5066398d
mccrush <mccrush2027@gmail.com>
2025-01-21 12:07:44 +0700

    Add structure files

--- src/.vitepress/config.mjs
+++ src/.vitepress/config.mjs
@@ -9,7 +9,9 @@ export default defineConfig({
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: 'Examples', link: '/markdown-examples' },
+      { text: 'Руководство', link: '/doc' },
+      { text: 'Соглашение', link: '/eula' }
     ],
 
     sidebar: [
@@ -18,6 +20,25 @@ export default defineConfig({
         items: [
           { text: 'Markdown Examples', link: '/markdown-examples' },
           { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      },
+      {
+        text: 'Руководство',
+        items: [
+          { text: 'Введение', link: '/doc/vvedenie' },
+          { text: 'Верхняя панель навигации', link: '/doc/panel' },
+          { text: 'Pro ', link: '/doc/pro' },
+          { text: 'Работа с проектами', link: '/doc/projects' },
+          { text: 'Настройки аккаунта', link: '/doc/accaunt' },
+          { text: 'Сброс и восстановление пароля', link: '/doc/password' }
+        ]
+      },
+      {
+        text: 'Соглашение',
+        items: [
+          { text: 'Общие положения', link: '/eula/obshee' },
+          { text: 'Аккаунт и данные пользователя', link: '/eula/accaunt' },
+          { text: 'Условия и порядок оплаты', link: '/eula/uslovia' }
         ]
       }
     ],
